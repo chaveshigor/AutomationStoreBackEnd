67878d04c9ca9c0bc5e4b46be3d971f996935580
Higor <hhigorsilvachaves@gmail.com>
2021-08-04 23:07:11 -0300

    client routes added

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express';
 
+import { ensureAuth } from '../middlewares/ensureAuth';
+import { CreateClientController } from '../modules/client/useCases/createClient/createClientController';
+
 const clientsRoutes = Router();
+
+clientsRoutes.post('/',
+  ensureAuth,
+  new CreateClientController().handle);
 
 export { clientsRoutes };
