86aa7b7f85119569b02dd581e92fdf0568d02714
Higor <hhigorsilvachaves@gmail.com>
2021-08-08 23:49:05 -0300

    running migrations before tests

--- src/__tests__/utils/runBeforeTests.ts
+++ src/__tests__/utils/runBeforeTests.ts
@@ -3,6 +3,7 @@ import { setNewConnection } from '../../database/connection';
 async function runBeforeAll() {
   const connection = await setNewConnection();
   await connection.runMigrations();
+  await connection.close();
 }
 
-runBeforeAll();
+export default runBeforeAll;
