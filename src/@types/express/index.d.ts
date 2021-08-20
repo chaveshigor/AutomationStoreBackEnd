3c91f084f831364b6e03b8796d18bf3322d39d67
Higor <hhigorsilvachaves@gmail.com>
2021-08-20 15:23:33 -0300

    adding the user plan to the requisition

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -3,8 +3,7 @@ declare namespace Express {
     export interface Request {
       user: {
         id: string;
-        admin: boolean;
-        company_id: string;
+        plan: string
       };
     }
   }
